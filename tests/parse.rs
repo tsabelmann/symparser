9504f09171de37fd93aea7d565c13099fb24a724
tsabelmann <tim.sabelmann@hotmail.de>
2022-03-15 10:08:21 +0100

    init: Added parse.rs

---
+++ tests/parse.rs
@@ -0,0 +1,25 @@
+extern crate pest;
+
+use pest::Parser;
+use std::fs::read_to_string;
+use symparser::{Rule, SymParser};
+
+#[test]
+fn parse_001() {
+    let string = read_to_string("examples/example_001.sym").unwrap();
+    let pairs = SymParser::parse(Rule::sym, &string).unwrap();
+    for pair in pairs {
+        println!("Rule:    {:?}", pair.as_rule());
+        // println!("Span:    {:?}", pair.as_span());
+        // println!("Text:    {}", pair.as_str());
+
+        // A pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            match inner_pair.as_rule() {
+                Rule::format_version => println!("FormatVersion: {}", inner_pair.as_str()),
+                Rule::title => println!("Title: {}", inner_pair.as_str()),
+                _ => continue,
+            };
+        }
+    }
+}
