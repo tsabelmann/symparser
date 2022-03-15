e9dcf22395580445f7f853e43b8cc939ac5fb4d5
tsabelmann <tim.sabelmann@hotmail.de>
2022-03-15 12:14:53 +0100

    feat: Added tests

--- tests/parse_002.rs
+++ tests/parse_002.rs
@@ -1,4 +1,4 @@
-use pest::Parser;
+use pest::{Parser, RuleType};
 use std::fs::read_to_string;
 use symparser::v6::{Rule, SymParser};
 
@@ -11,5 +11,29 @@ fn parse_001() {
 #[test]
 fn parse_002() {
     let string = read_to_string("examples/example_002.sym").unwrap();
-    let _pairs = SymParser::parse(Rule::sym, &string).unwrap();
+    let mut pairs = SymParser::parse(Rule::sym, &string).unwrap().into_iter();
+    assert_eq!(pairs.next().is_some(), true);
+    assert_eq!(pairs.next().is_some(), false);
+}
+
+#[test]
+fn parse_003() {
+    let string = read_to_string("examples/example_002.sym").unwrap();
+    let mut pairs = SymParser::parse(Rule::sym, &string).unwrap().into_iter();
+    let mut inner_pair = pairs.next().unwrap().into_inner();
+
+    assert_eq!(inner_pair.next().unwrap().as_str(), "FormatVersion=6.0");
+    assert_eq!(inner_pair.next().unwrap().as_str(), "Title=\"EXAMPLE\"");
+    assert_eq!(inner_pair.next(), None);
+}
+
+#[test]
+fn parse_004() {
+    let string = read_to_string("examples/example_002.sym").unwrap();
+    let mut pairs = SymParser::parse(Rule::sym, &string).unwrap().into_iter();
+    let mut inner_pair = pairs.next().unwrap().into_inner();
+
+    assert_eq!(inner_pair.next().unwrap().as_rule(), Rule::format_version);
+    assert_eq!(inner_pair.next().unwrap().as_rule(), Rule::title);
+    assert_eq!(inner_pair.next(), None);
 }
