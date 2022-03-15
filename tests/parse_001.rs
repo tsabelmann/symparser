16db4368764f8a928a8bcbc356add5602e41e230
tsabelmann <tim.sabelmann@hotmail.de>
2022-03-15 12:16:08 +0100

    feat: Added tests

--- tests/parse_001.rs
+++ tests/parse_001.rs
@@ -1,27 +1,29 @@
-extern crate pest;
-
-use pest::Parser;
+use pest::{Parser, RuleType};
 use std::fs::read_to_string;
 use symparser::v6::{Rule, SymParser};
-
 
 #[test]
 fn parse_001() {
     let string = read_to_string("examples/example_001.sym").unwrap();
-    let pairs = SymParser::parse(Rule::sym, &string).unwrap();
-    for pair in pairs {
-        println!("Rule:    {:?}", pair.as_rule());
-        // println!("Span:    {:?}", pair.as_span());
-        // println!("Text:    {}", pair.as_str());
-
-        // A pair can be converted to an iterator of the tokens which make it up:
-        for inner_pair in pair.into_inner() {
-            match inner_pair.as_rule() {
-                Rule::format_version => println!("FormatVersion: {}", inner_pair.as_str()),
-                Rule::title => println!("Title: {}", inner_pair.as_str()),
-                _ => continue,
-            };
-        }
-    }
+    let _pairs = SymParser::parse(Rule::sym, &string).unwrap();
 }
 
+// #[test]
+// fn parse_002() {
+//     let string = read_to_string("examples/example_001.sym").unwrap();
+//     let pairs = SymParser::parse(Rule::sym, &string).unwrap();
+//     for pair in pairs {
+//         println!("Rule:    {:?}", pair.as_rule());
+//         // println!("Span:    {:?}", pair.as_span());
+//         // println!("Text:    {}", pair.as_str());
+//
+//         // A pair can be converted to an iterator of the tokens which make it up:
+//         for inner_pair in pair.into_inner() {
+//             match inner_pair.as_rule() {
+//                 Rule::format_version => println!("FormatVersion: {}", inner_pair.as_str()),
+//                 Rule::title => println!("Title: {}", inner_pair.as_str()),
+//                 _ => continue,
+//             };
+//         }
+//     }
+// }
