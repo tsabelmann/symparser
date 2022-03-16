37e73617b1ce01655edd6b4e3cc65910dfdb6da5
tsabelmann <tim.sabelmann@hotmail.de>
2022-03-16 15:53:27 +0100

    feat: Changed parsing rule

--- tests/parse_002.rs
+++ tests/parse_002.rs
@@ -5,13 +5,13 @@ use symparser::v6::{Rule, SymParser};
 #[test]
 fn parse_001() {
     let string = read_to_string("examples/example_002.sym").unwrap();
-    let _pairs = SymParser::parse(Rule::sym, &string).unwrap();
+    let _pairs = SymParser::parse(Rule::main, &string).unwrap();
 }
 
 #[test]
 fn parse_002() {
     let string = read_to_string("examples/example_002.sym").unwrap();
-    let mut pairs = SymParser::parse(Rule::sym, &string).unwrap().into_iter();
+    let mut pairs = SymParser::parse(Rule::main, &string).unwrap().into_iter();
     assert_eq!(pairs.next().is_some(), true);
     assert_eq!(pairs.next().is_some(), false);
 }
@@ -19,7 +19,7 @@ fn parse_002() {
 #[test]
 fn parse_003() {
     let string = read_to_string("examples/example_002.sym").unwrap();
-    let mut pairs = SymParser::parse(Rule::sym, &string).unwrap().into_iter();
+    let mut pairs = SymParser::parse(Rule::main, &string).unwrap().into_iter();
     let mut inner_pair = pairs.next().unwrap().into_inner();
 
     assert_eq!(
@@ -36,7 +36,7 @@ fn parse_003() {
 #[test]
 fn parse_004() {
     let string = read_to_string("examples/example_002.sym").unwrap();
-    let mut pairs = SymParser::parse(Rule::sym, &string).unwrap().into_iter();
+    let mut pairs = SymParser::parse(Rule::main, &string).unwrap().into_iter();
     let mut inner_pair = pairs.next().unwrap().into_inner();
 
     assert_eq!(inner_pair.next().unwrap().as_rule(), Rule::format_version);
