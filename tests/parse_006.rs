d325f1918139bcbf4534d0323575fedc4bf15da5
tsabelmann <tim.sabelmann@hotmail.de>
2022-03-17 17:57:30 +0100

    init: Added parse_006.rs

---
+++ tests/parse_006.rs
@@ -0,0 +1,9 @@
+use pest::Parser;
+use std::fs::read_to_string;
+use symparser::v6::{Rule, SymParser};
+
+#[test]
+fn parse_001() {
+    let string = read_to_string("examples/example_006.sym").unwrap();
+    let _pairs = SymParser::parse(Rule::main, &string).unwrap();
+}
